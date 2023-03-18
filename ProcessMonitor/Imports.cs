1108a54d4b40ed2693a41685f1d6ee66ba58b8e2
Niels Breuker <niels.breuker@hotmail.nl>
2023-03-18 22:57:48 +0100

    Add project files.

---
+++ ProcessMonitor/Imports.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessMonitor
+{
+	internal class Imports
+	{
+		// To support flashing.
+		[DllImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool FlashWindowEx(ref FLASHWINFO pwfi);
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct FLASHWINFO
+		{
+			public UInt32 cbSize;
+			public IntPtr hwnd;
+			public UInt32 dwFlags;
+			public UInt32 uCount;
+			public UInt32 dwTimeout;
+		}
+
+		// stop flashing
+		const int FLASHW_STOP = 0;
+
+		// flash the window title
+		const int FLASHW_CAPTION = 1;
+
+		// flash the taskbar button
+		const int FLASHW_TRAY = 2;
+
+		const int FLASHW_ALL = 3;
+
+		// flash continuously
+		const int FLASHW_TIMER = 4;
+
+		// flash until the window comes to the foreground
+		const int FLASHW_TIMERNOFG = 12;
+
+
+
+
+		// Do the flashing - this does not involve a raincoat.
+		public static bool FlashWindowEx()
+		{
+			IntPtr hWnd = Process.GetCurrentProcess().MainWindowHandle;
+			FLASHWINFO fInfo = new FLASHWINFO();
+
+			fInfo.cbSize = Convert.ToUInt32(Marshal.SizeOf(fInfo));
+			fInfo.hwnd = hWnd;
+			fInfo.dwFlags = FLASHW_ALL | FLASHW_TIMERNOFG;
+			fInfo.uCount = UInt32.MaxValue;
+			fInfo.dwTimeout = 0;
+
+			return FlashWindowEx(ref fInfo);
+		}
+
+		public static bool StopFlash()
+		{
+			IntPtr hWnd = Process.GetCurrentProcess().MainWindowHandle;
+			FLASHWINFO fInfo = new FLASHWINFO();
+
+			fInfo.cbSize = Convert.ToUInt32(Marshal.SizeOf(fInfo));
+			fInfo.hwnd = hWnd;
+			fInfo.dwFlags = 0;
+			fInfo.uCount = UInt32.MaxValue;
+			fInfo.dwTimeout = 0;
+
+			return FlashWindowEx(ref fInfo);
+		}
+	}
+}
---
+++ ProcessMonitor/Program.cs
@@ -0,0 +1,70 @@
+﻿//using Microsoft.WindowsAPICodePack.Taskbar;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessMonitor
+{
+	public class Program
+	{
+		/// <summary>
+		/// Logs the provided text with a timestamp as a prefix.
+		/// </summary>
+		/// <param name="text"></param>
+		static void Log(string text)
+		{
+			var timestamp = DateTime.Now.AddHours(-16).ToString("HH:mm");
+			Console.WriteLine($"[{timestamp}]: {text}");
+		}
+
+		public static void Main(string[] args)
+		{
+			var processName = args.ElementAtOrDefault(0);
+			while (string.IsNullOrEmpty(processName))
+			{
+				Log("No process name was provided.");
+				Log("What is the name of the process?");
+				processName = Console.ReadLine();
+			}
+
+			Log($"Monitoring {processName}");
+
+			bool processFound = false;
+			while (true)
+			{
+				var process = FindProcess(processName);
+				if (processFound && (process == null))
+				{
+					Log("Process not found.\a");
+					Imports.FlashWindowEx();
+					processFound = false;
+				}
+				else if (!processFound && (process != null))
+				{
+					Log("Process found.");
+					Imports.StopFlash();
+					processFound = true;
+				}
+				Thread.Sleep(5000);
+			}
+		}
+
+		/// <summary>
+		/// Finds a process by executable name or window title.
+		/// </summary>
+		/// <param name="processName"></param>
+		/// <returns></returns>
+		private static Process FindProcess(string processName)
+		{
+			return Process.GetProcesses().FirstOrDefault(x =>
+				x.ProcessName.ToLower().Contains(processName.ToLower()) ||
+				x.MainWindowTitle.ToLower().Contains(processName.ToLower())
+			);
+		}
+	}
+}
